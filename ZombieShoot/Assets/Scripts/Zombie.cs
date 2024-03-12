b94558daad6577ee8b792ecdb8b8988a1d8e918e
Oxnack <128256361+Oxnack@users.noreply.github.com>
2024-03-12 16:33:05 +0300

    UpdateZombieCs

--- ZombieShoot/Assets/Scripts/Zombie.cs
+++ ZombieShoot/Assets/Scripts/Zombie.cs
@@ -1,33 +1,58 @@
+using PlayerController;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Zombie : MonoBehaviour
+namespace Enemy
 {
-    [SerializeField] private float _speed = 5f; // Скорость перемещения
-
-    private Transform target; // Ссылка на целевой объект
-    private Rigidbody rb;
-
-    void Start()
+    public class Zombie : MonoBehaviour             //сорян что без солида, неуспел, дедлайн сгорел
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
-        rb = GetComponent<Rigidbody>();
+        [SerializeField] private float _speed = 5f; // Скорость перемещения
+
+        private Transform _target; // Ссылка на целевой объект
+        private Rigidbody _rb;
+
+        void Start()
+        {
+            _target = GameObject.FindGameObjectWithTag("Player").transform;
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            Vector3 targetPosition = new Vector3(_target.position.x, transform.position.y, _target.position.z); // Устанавливаем позицию цели по x и z
+            Vector3 lookDirection = (targetPosition - transform.position).normalized; // Нормализуем вектор направления взгляда
+
+            // Поворачиваем объект в направлении цели только по осям x и z
+            transform.LookAt(transform.position + lookDirection, Vector3.up);
+
+
+            Vector3 moveDirection = transform.forward;
+
+            // Двигаем объект с помощью Rigidbody.MovePosition
+            _rb.MovePosition(_rb.position + moveDirection * _speed * Time.deltaTime);
+        }
     }
 
-    void Update()
+    public class Attack
     {
-        Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, target.position.z); // Устанавливаем позицию цели по x и z
-        Vector3 lookDirection = (targetPosition - transform.position).normalized; // Нормализуем вектор направления взгляда
+        public Transform transform;
+        public GameObject player;
 
-        // Поворачиваем объект в направлении цели только по осям x и z
-        transform.LookAt(transform.position + lookDirection, Vector3.up);
+        public void GetAttack(float distance, int damage)
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) < distance)
+            { 
+                player.GetComponent<Player>().Hp.GetAttack(damage);
+            }
+        }
 
-
-        Vector3 moveDirection = transform.forward;
-
-        // Двигаем объект с помощью Rigidbody.MovePosition
-        rb.MovePosition(rb.position + moveDirection * _speed * Time.deltaTime);
+        private IEnumerator GetDamageByTime(float time, float distance)
+        {
+            yield return new WaitForSeconds(time);
+            while ()
+        }
     }
 }
 
